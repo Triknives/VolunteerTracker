bd0b5c6b1c5fa73d6f4d02a1d3ec39986a99c3ae
Student <example@example.com>
2020-01-10 13:59:24 -0800

    update app rb and routing

--- app.rb
+++ app.rb
@@ -38,121 +38,57 @@ end
 
 get ('/projects/:id') do
   @project = Project.find(params[:id].to_i())
-  if @album != nil
-    erb(:album)
+  if @project != nil
+    erb(:project)
   else
-    erb(:album_error)
+    erb(:project_error)
   end
 end
 
-get ('/albums/:id/edit') do
-  @album = Album.find(params[:id].to_i())
-  erb(:edit_album)
+get ('/projects/:id/edit') do
+  @project = Project.find(params[:id].to_i())
+  erb(:edit_project)
 end
 
-patch ('/albums/:id') do
-  @album = Album.find(params[:id].to_i())
-  @album.update(params[:name])
-  redirect to('/albums')
+patch ('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  @project.update(params[:name])
+  redirect to('/projects')
 end
 
-delete ('/albums/:id') do
-  @album = Album.find(params[:id].to_i())
-  @album.delete()
-  redirect to('/albums')
+delete ('/projects/:id') do
+  @project = Project.find(params[:id].to_i())
+  @project.delete()
+  redirect to('/projects')
 end
 
-get ('/albums/:id/songs/:song_id') do
-  @song = Song.find(params[:song_id].to_i())
-  if @song != nil
-    erb(:song)
+get ('/projects/:id/volunteers/:volunteer_id') do
+  @volunteer= Volunteer.find(params[:volunteer_id].to_i())
+  if @volunteer != nil
+    erb(:volunteer)
   else
-    @album = Album.find(params[:id].to_i())
-    erb(:album_error)
+    @project = Project.find(params[:id].to_i())
+    erb(:project_error)
   end
 end
 
-post ('/albums/:id/songs') do
-  @album = Album.find(params[:id].to_i())
-  song = Song.new({:name => params[:song_name], :album_id => @album.id, :id => nil})
-  song.save()
-  erb(:album)
+post ('/projects/:id/volunteers') do
+  @project = Project.find(params[:id].to_i())
+  volunteer = Volunteer.new({:name => params[:volunteer_name], :project_id => @project.id, :id => nil})
+  volunteer.save()
+  erb(:project)
 end
 
-patch ('/albums/:id/songs/:song_id') do
-  @album = Album.find(params[:id].to_i())
-  song = Song.find(params[:song_id].to_i())
-  song.update(params[:name], @album.id)
-  erb(:album)
+patch ('/projects/:id/volunteers/:volunteer_id') do
+  @project = Project.find(params[:id].to_i())
+  volunteer = Volunteer.find(params[:volunteer_id].to_i())
+  volunteer.update(params[:volunteer_name], @project.id)
+  erb(:project)
 end
 
-delete ('/albums/:id/songs/:song_id') do
-  song = Song.find(params[:song_id].to_i())
-  song.delete
-  @album = Album.find(params[:id].to_i())
-  erb(:album)
-end
-
-# ARTIST ROUTING:
-
-get('/artists') do
-  if params["search"]
-    @artist = Artist.search(params[:search])
-  elsif params["sort"]
-    @artist = Artist.sort()
-  else
-    @artist = Artist.all
-  end
-  erb(:artists)
-end
-
-get ('/artists/:id') do
-  @artist = Artist.find(params[:id].to_i())
-  if @artist != nil
-    erb(:artist)
-  else
-    erb(:album_error)
-  end
-end
-
-get ('/artist/new') do
-  erb(:new_artist)
-end
-
-post ('/artists') do
-  name = params[:artist_name]
-  @artist = Artist.new({:name => name, :id => nil})
-  @artist.save()
-  redirect to('/artists')
-end
-
-# post ('/artists/:id') do
-#   name = params[:artist_name]
-#   @artist = Artist.new({:name => name, :id => nil})
-#   @artist.save()
-#   redirect to('/artists')
-# end
-
-
-post ('/artists/:id') do
-  name = params[:album_name]
-  id = params[:id]
-  @artist = Artist.find(params[:id].to_i())
-  album = Album.new({:name => name, :id => id})
-  album.save()
-  @artist.update({:album_name => name})
-  redirect to('/artists')
-end
-
-
-patch ('/artists/:id') do
-  @artist = Artist.find(params[:id].to_i())
-  @artist.update(params[:name])
-  redirect to('/artists')
-end
-
-delete ('/artists/:id') do
-  @artist = Artist.find(params[:id].to_i())
-  @artist.delete()
-  redirect to('/artists')
+delete ('/projects/:id/volunteers/:volunteer_id') do
+  volunteer = Volunteer.find(params[:volunteer_id].to_i())
+  volunteer.delete
+  @project = Project.find(params[:id].to_i())
+  erb(:project)
 end
