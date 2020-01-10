11fdac04f23e7c09e576a5982aaa793d2614dd35
Student <example@example.com>
2020-01-10 12:19:49 -0800

    update Volunteer class

--- lib/volunteers.rb
+++ lib/volunteers.rb
@@ -1,75 +1,75 @@
-class Song
-  attr_accessor :name, :album_id, :id
+class Volunteer
+  attr_accessor :volunteer_name, :project_id, :id
 
   def initialize(attributes)
-    @name = attributes.fetch(:name)
-    @album_id = attributes.fetch(:album_id)
+    @volunteer_name = attributes.fetch(:volunteer_name)
+    @album_id = attributes.fetch(:project_id)
     @id = attributes.fetch(:id)
   end
 
-  def ==(song_to_compare)
-    if song_to_compare != nil
-      (self.name() == song_to_compare.name()) && (self.album_id() == song_to_compare.album_id())
+  def ==(volunteer_to_compare)
+    if volunteer_to_compare != nil
+      (self.volunteer_name() == volunteer_to_compare.volunteer_name()) && (self.project_id() == song_to_compare.project_id())
     else
       false
     end
   end
 
   def self.all
-    returned_songs = DB.exec("SELECT * FROM songs;")
-    songs = []
-    returned_songs.each() do |song|
-      name = song.fetch("name")
-      album_id = song.fetch("album_id").to_i
-      id = song.fetch("id").to_i
-      songs.push(Song.new({:name => name, :album_id => album_id, :id => id}))
+    returned_volunteers = DB.exec("SELECT * FROM volunteers;")
+    volunteers = []
+    returned_volunteers.each() do |volunteer|
+      volunteer_name = volunteer.fetch("volunteer_name")
+      project_id = volunteer.fetch("project_id").to_i
+      id = volunteer.fetch("id").to_i
+      volunteers.push(Volunteer.new({:volunteer_name => volunteer_name, :project_id => project_id, :id => id}))
     end
-    songs
+    volunteers
   end
 
   def save
-    result = DB.exec("INSERT INTO songs (name, album_id) VALUES ('#{@name}', #{@album_id}) RETURNING id;")
+    result = DB.exec("INSERT INTO volunteers (volunteer_name, project_id) VALUES ('#{@volunteer_name}', #{@project_id}) RETURNING id;")
     @id = result.first().fetch("id").to_i
   end
 
   def self.find(id)
-    song = DB.exec("SELECT * FROM songs WHERE id = #{id};").first
+    volunteer = DB.exec("SELECT * FROM volunteers WHERE id = #{id};").first
     if song
-      name = song.fetch("name")
-      album_id = song.fetch("album_id").to_i
-      id = song.fetch("id").to_i
-      Song.new({:name => name, :album_id => album_id, :id => id})
+      volunteer_name = volunteer.fetch("volunteer_name")
+      project_id = volunteer.fetch("project_id").to_i
+      id = volunteer.fetch("id").to_i
+      Volunteer.new({:volunteer_name => volunteer_name, :project_id => project_id, :id => id})
     else
       nil
     end
   end
 
-  def update(name, album_id)
-    @name = name
-    @album_id = album_id
-    DB.exec("UPDATE songs SET name = '#{@name}', album_id = #{@album_id} WHERE id = #{@id};")
+  def update(volunteer_name, project_id)
+    @volunteer_name = volunteer_name
+    @project_id = project_id
+    DB.exec("UPDATE volunteers SET name = '#{@volunteer_name}', project_id = #{@project_id} WHERE id = #{@id};")
   end
 
   def delete
-    DB.exec("DELETE FROM songs WHERE id = #{@id};")
+    DB.exec("DELETE FROM volunteers WHERE id = #{@id};")
   end
 
   def self.clear
-    DB.exec("DELETE FROM songs *;")
+    DB.exec("DELETE FROM volunteers *;")
   end
 
-  def self.find_by_album(alb_id)
-    songs = []
-    returned_songs = DB.exec("SELECT * FROM songs WHERE album_id = #{alb_id};")
-    returned_songs.each() do |song|
-      name = song.fetch("name")
-      id = song.fetch("id").to_i
-      songs.push(Song.new({:name => name, :album_id => alb_id, :id => id}))
+  def self.find_by_project(prj_id)
+    volunteers = []
+    returned_volunteers = DB.exec("SELECT * FROM volunteers WHERE album_id = #{prj_id};")
+    returned_volunteers.each() do |song|
+      volunteer_name = volunteer.fetch("volunteer_name")
+      id = volunteer.fetch("id").to_i
+      volunteers.push(Volunteer.new({:volunteer_name => volunteer_name, :project_id => prj_id, :id => id}))
     end
-    songs
+    volunteers
   end
 
-  def album
-    Album.find(@album_id)
+  def projects
+    Project.find(@project_id)
   end
 end
